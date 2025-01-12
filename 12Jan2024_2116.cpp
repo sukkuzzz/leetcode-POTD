516de483bd675403c5fc74dcd2511773f4aeb513
Sukriti Sharma <sukriti.sharma.met21@itbhu.ac.in>
2025-01-12 22:16:10 +0530

    12 Jan 2025

---
+++ 12Jan2024_2116.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+const int M = 1e9 + 7;
+void solve()
+{
+    string s, locked;
+    cin >> s >> locked;
+    cout << canBeValid(s, locked) << endl;
+}
+bool canBeValid(string s, string locked)
+{
+}
+int main()
+{
+    int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ 8Jan2024_3042.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+const int M = 1e9 + 7;
+void solve() {}
+
+int main()
+{
+}
